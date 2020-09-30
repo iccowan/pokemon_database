2b57092bf316d31cad12af1d395a617ed0d22a2c
Gus Crow <gus.crow@centre.edu>
2020-09-30 08:46:27 -0400

    Add new file

---
+++ Implementation/SQL/scripts/pokemon_scripts/add_pokemon_used.sql
@@ -0,0 +1,7 @@
+/*  This can be used to create a record in the "pokemonused_for_challenges" table
+*   For a prepared statement, the following values should be used:
+*   'i,i,i'
+*/
+
+INSERT INTO pokemon_used_for_challenges (pokemon_challenge_id, pokemon_id. challenge_id)
+VALUES (?, ?, ?);
