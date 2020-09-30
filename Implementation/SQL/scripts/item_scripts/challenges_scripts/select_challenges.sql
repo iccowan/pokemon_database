0530f1216cdaa3c1db6325c6ed9d0c4a0cf6040a
Ben <ben.hodges@centre.edu>
2020-09-29 20:43:54 -0400

    Update select_challenges.sql

--- Implementation/SQL/scripts/item_scripts/challenges_scripts/select_challenges.sql
+++ Implementation/SQL/scripts/item_scripts/challenges_scripts/select_challenges.sql
@@ -1,3 +1,6 @@
-/*Select challenges*/
+/*This can be used to view all records from the "trainers" table
+*   For a prepared statement, the following values should be used:
+*   'i'
+*/
 
 SELECT * FROM challenges WHERE challenge_id = ?;
