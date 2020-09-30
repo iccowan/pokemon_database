5b3f70b37af5ff0a2f84e910a218599b4c3b7349
Ben <ben.hodges@centre.edu>
2020-09-29 20:48:44 -0400

    Add new file

---
+++ Implementation/SQL/scripts/item_scripts/challenges_scripts/add_challenge.sql
@@ -0,0 +1,6 @@
+/*  This can be used to create a record in the "challenges" table
+*   For a prepared statement, the following values should be used:
+*   'i,i'
+*/
+
+INSERT INTO challenges (challenge_id,trainer_id) VALUES (?,?);
