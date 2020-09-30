9334eb784fa9d97c31640f67b55bad3515261cf5
Ben <ben.hodges@centre.edu>
2020-09-29 20:46:34 -0400

    Add new file

---
+++ Implementation/SQL/scripts/item_scripts/challenges_scripts/delete_challenge.sql
@@ -0,0 +1,5 @@
+This can be used to delete a record from the "challenges" table
+*   For a prepared statement, the following value should be used:
+*   'i'
+
+DELETE FROM challenges WHERE challenge_id = ?;
