61c691dcc9a7a7603dee4d231c5377b60acd66a4
Ian Cowan <ian@cowanemail.com>
2020-10-06 19:41:51 -0400

    Debug update items

--- Implementation/PHP/items.php
+++ Implementation/PHP/items.php
@@ -9,6 +9,11 @@ error_reporting(E_ALL);
 
 <head>
     <title>Pokemon League Items</title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
     <h1>Pokemon League Items</h1>
