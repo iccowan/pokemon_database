4b2ea1edcbf013e7495c1829b31593bc3be5d1c7
Ian Cowan <ian@cowanemail.com>
2020-10-08 21:03:10 -0400

    Fix tables

--- Implementation/SQL/pokemon.sql
+++ Implementation/SQL/pokemon.sql
@@ -4,7 +4,7 @@ CREATE TABLE pokemon (
     PRIMARY KEY (pokemon_id),
     pokemon_id          INT unsigned    NOT NULL AUTO_INCREMENT,
     pokemon_name        VARCHAR(32)     NOT NULL,
-    pokemon_type_id     INT             NOT NULL,
+    pokemon_type_id     INT unsigned    NOT NULL,
     pokemon_level       INT             NOT NULL,
     trainer_id          INT unsigned    NOT NULL,
     FOREIGN KEY (trainer_id)      REFERENCES trainers     (trainer_id),
